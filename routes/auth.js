7d8af45b17a5daf80db49c927b2b28f85c0b8667
Chantal <chantalgomez@pursuit.org>
2024-10-18 21:56:46 -0400

    refactor:auth file changed. routes for password reset are now under passwordReset.js file

--- routes/auth.js
+++ routes/auth.js
@@ -31,10 +31,6 @@ const {
   trackFailedLoginByUser,
   trackFailedLoginByIP,
 } = require("../utils/loginTracker");
-const {
-  transporter,
-  createResetPasswordEmail,
-} = require("../utils/nodeMailer");
 
 const { validateTOTP, registerTOTP } = require("../utils/TOTP");
 
@@ -189,69 +185,5 @@ router.post("/logout", verifyToken, async (req, res) => {
   }
 });
 
-router.post("/forgot-password", async (req, res) => {
-  const { email } = req.body;
-
-  try {
-    const findUserQuery = "SELECT * FROM users WHERE email = $1";
-    const userResult = await db.query(findUserQuery, [email]);
-
-    if (userResult.length === 0) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    const resetToken = crypto.randomBytes(20).toString("hex");
-    const resetTokenExpiry = new Date(Date.now() + 900000); // Token expires in 15 mins
-
-    const insertResetTokenQuery =
-      "INSERT INTO reset_tokens (email, token, expiration_time) VALUES ($1, $2, $3)";
-    await db.query(insertResetTokenQuery, [
-      email,
-      resetToken,
-      resetTokenExpiry,
-    ]);
-
-    const resetUrl = `https://authenticatorrrr.netlify.app/reset-password?token=${resetToken}`;
-    await transporter.sendMail(createResetPasswordEmail(email, resetUrl));
-
-    res.json({ message: "Password reset link sent to your email" });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error processing request", error: error.message });
-  }
-});
-
-// Reset password route
-router.post("/reset-password", validatePassword, async (req, res) => {
-  const { password } = req.body;
-  const token = req.query.token;
-  console.log("token: " + token);
-
-  try {
-    const checkTokenQuery =
-      "SELECT * FROM reset_tokens WHERE token = $1 AND expiration_time > NOW()";
-    const tokenResult = await db.query(checkTokenQuery, [token]);
-
-    if (tokenResult.length === 0) {
-      return res.status(400).json({ message: "Invalid or expired token" });
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const updatePasswordQuery =
-      "UPDATE users SET password = $1 WHERE email = $2";
-    await db.query(updatePasswordQuery, [hashedPassword, tokenResult[0].email]);
-
-    const clearTokenQuery = "DELETE FROM reset_tokens WHERE token = $1";
-    await db.query(clearTokenQuery, [token]);
-
-    res.json({ message: "Password reset successful" });
-  } catch (error) {
-    res.status(500).json({
-      message: "Server error during password reset",
-      error: error.message,
-    });
-  }
-});
 
 module.exports = router;
--- routes/movies.js
+++ routes/movies.js
@@ -3,21 +3,17 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const db = require("../db/db");
 const router = express.Router();
-const JWT_SECRET = process.env.JWT_SECRET;
-const crypto = require("crypto");
-const redisClient = require("redis").createClient();
 
 
 const {
   verifyToken,
 } = require("../middleware/jwt-authorization");
-const { apiLimiter} = require("../middleware/rateLimiter")
 
-router.get( "/", verifyToken, async (req, res) => {
-      res.status(200).send("movie route! can only be reached with valid token");
+router.get( "/", verifyToken,  async (req, res) => {
+      res.status(200).send("movie route! can only be reached with valid token!");
 })
-router.get("/no-token", verifyToken, async (req, res) => {
-  res.status(200).send("movie route!!! no token needed");
+router.get("/no-token", async (req, res) => {
+  res.status(200).send("movie route with no token needed!!!");
 });
 
 module.exports = router;
