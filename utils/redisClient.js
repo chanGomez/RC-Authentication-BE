05f4f7da7e055ff3ecdabf4226c8201c947de901
Chantal <chantalgomez@pursuit.org>
2024-10-24 13:27:31 -0400

    deploy: redis configuration changed to increase connection timeout

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -2,6 +2,9 @@ const redis = require("redis");
 
 const redisClient = redis.createClient({
   url: process.env.REDIS_URL,
+  socket: {
+    connectTimeout: 10000, // timeout in milliseconds, increase if needed
+  },
 });
 
 redisClient.on("error", (err) => {
