501d4a29909c4430230cbba8e643687bc4ed82f5
Chantal <chantalgomez@pursuit.org>
2024-10-31 12:37:44 -0400

    fix: cors allowed origins function created errors, was removed for deployment but will be implemented later.

--- app.js
+++ app.js
@@ -2,21 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const cookieParser = require("cookie-parser");
-require("dotenv").config();
-const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200, // For legacy browser support
-};
 app.use(cookieParser());
-app.use(cors(corsOptions));
+require("dotenv").config();
+app.use(cors());
 app.use(express.json());
 
 const authRouter = require("./controllers/authController");
@@ -35,5 +24,6 @@ app.use("/movies", movieRouter);
 app.use("*", (err, req, res, next) => {
   res.status(500).send("Something went wrong!");
 });
+
 
 module.exports = app;
