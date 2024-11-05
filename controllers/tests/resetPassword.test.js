b71f36a41728205c621687e32ae2783396cd161c
Chantal <chantalgomez@pursuit.org>
2024-11-05 15:59:50 -0500

    test: starter test for reset password toute created

---
+++ controllers/tests/resetPassword.test.js
@@ -0,0 +1,19 @@
+const supertest = require("supertest");
+const app = require("../../app.js");
+const db = require("../../db/db");
+
+
+describe("POST /forgot-password", () => {
+  it("should create a reset token and send an email to the user", async () => {
+
+    const response = await supertest(app).post("/auth/forgot-password").send({
+      email: "user1@example.com"
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe(
+      "Reset toke created and email with rest password link sent."
+    );
+
+  });
+});
